9743a9638d85300ba35f1c95d579be76a310b224
Allen <leander_lightlord99@hotmail.com>
2023-09-28 09:36:23 +0200

    minor beauty edits to code.

--- config/database.config.php
+++ config/database.config.php
@@ -1,23 +1,20 @@
-<?php
-class Database {
-  private $host = 'localhost';
-  private $user = 'root';
-  private $password = '';
-  private $database = 'donkeytra_db';
-
-  
-  function connect() {
-   try { 
-    $dsn = 'mysql:host=' . $this->host .
-            ';dbname=' . $this->database;
-    $pdo = new PDO($dsn, $this->user, $this->password);
-    $doeIets = $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    return $doeIets; 
-    echo "hello allemaal!";
-    }  
-    catch(PDOException $e){
-        throw new Exception("Failed to connectto the database." . $e->getMessage());
-    }     
+<?php //Loubna Faress
+  class Database {
+    private $host = 'localhost';
+    private $user = 'root';
+    private $password = '';
+    private $database = 'donkeytra_db';
+    
+    function connect() {
+      try { 
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->database;
+        $pdo = new PDO($dsn, $this->user, $this->password);
+        $doeIets = $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $doeIets; 
+      }  
+      catch(PDOException $e){
+          throw new Exception("Failed to connectto the database." . $e->getMessage());
+      }     
+    }
   }
-}
 ?>
