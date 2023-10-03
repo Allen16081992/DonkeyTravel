5ec6d5e7532092f9b3ebb5afa408d15f625aeb8b
loubna faress <9008427@student.zadkine.nl>
2023-10-03 12:15:26 +0200

    log out page test

---
+++ config/logOut.php
@@ -0,0 +1,17 @@
+<?php // Loubna Faress
+
+session_start();
+
+// Controleer of de gebruiker is ingelogd
+if (isset($_SESSION['gebruikersnaam'])) {
+    // Verwijder alle sessievariabelen
+    session_unset();
+
+    // Vernietig de sessie
+    session_destroy();
+}
+
+// Stuur de gebruiker terug naar de inlogpagina of een andere gewenste bestemming
+header("Location: index.php");
+exit;
+?>
