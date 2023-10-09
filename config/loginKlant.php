617ec688262eeb62d8bc3132d8949f0b09f4bf41
Loubna Faress <loubna@securelogin.nu>
2023-10-09 10:29:18 +0200

    login klant af

---
+++ config/loginKlant.php
@@ -0,0 +1,29 @@
+<?php // Loubna Faress
+    class klantLogin {
+        private $pdo;
+
+        public function loginKlant() {
+            try {
+            $stmt = $this->pdo->prepare('SELECT ID, Naam FROM klanten WHERE Naam = ? AND Wachtwoord = ?;');
+            $stmt->execute([$Naam, $Wachtwoord]);
+            $klantID = $stmt->fetch(PDO::FETCH_ASSOC); 
+        }   
+        catch (PDOException $e) {
+            die("Connectie mislukt:" . $e->getMessage());
+        }
+
+        $_SESSION['klant_id'] = $klantID['ID'];
+        $_SESSION['klant_naam'] = klantLogin($klantID['Naam']);
+        
+        }
+    }
+    
+    header('location: ../donkey_client.php');
+
+    exit();
+
+    if(isset($_POST['submit'])) {
+        $Naam = $_POST['Naam'];
+        $Wachtwoord = $_POST['Wachtwoord']
+    }
+?>
