32991bf1dcb8414abb9192d30f6bb852a1b14a72
Loubna Faress <loubna@securelogin.nu>
2023-10-09 09:45:22 +0200

    Read klant is af

---
+++ config/fetchKlant.php
@@ -0,0 +1,37 @@
+<?php // Loubna Faress
+    require_once 'classes/database.class.php';
+
+    class fetchKlant {
+        private $pdo;
+
+        public function __construct() {
+            $database = new Database();
+            $this->pdo = $database->connect();
+        }
+
+        public function fetchKlantInfo($table) {
+            try {
+                // Select table records
+                $stmt = $this->pdo->prepare("SHOW COLUMNS FROM $table");
+                $stmt->execute();
+                $columns = $stmt->fetchAll(PDO::FETCH_COLUMN);
+
+                $stmt = $this->pdo->prepare("SELECT * FROM $table");
+                $stmt->execute();
+                $records = $stmt->fetchAll();
+
+                return [
+                    'columns' => $columns,
+                    'records' => $records,
+                ];
+            } catch (PDOException $e) {
+                // Handle the exception (e.g., log the error or display a user-friendly message)
+                die("Error: " . $e->getMessage());
+            }
+        }
+    }
+
+     // Create an object from our class
+     $viewFk = new fetchKlant();
+     $allInfo = $viewFk->fetchKlantInfo('klanten');
+?>
