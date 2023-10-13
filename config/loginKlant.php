c5c83bf54ee2c370aa86824ddace25de2c7c14ed
loubna faress <9008427@student.zadkine.nl>
2023-10-13 13:36:08 +0200

    comments toegevoegd loginklant.php

--- config/loginKlant.php
+++ config/loginKlant.php
@@ -5,38 +5,54 @@
     class klantLogin {
         private $pdo;
 
+        // maakt database connectie
         public function __construct() {
             $db = new Database();
             $this->pdo = $db->connect();
         }
 
+        // zoekt klant uit de database,  
         public function loginKlant($Naam, $Wachtwoord) {
             try {
                 $stmt = $this->pdo->prepare('SELECT ID, Naam FROM klanten WHERE Naam = ? AND Wachtwoord = ?;');
+
+                // voert het uit,
                 $stmt->execute([$Naam, $Wachtwoord]);
+
+                // haalt het op.
                 $klantID = $stmt->fetch(PDO::FETCH_ASSOC);
+
+                // geeft error als het mislukt
             } catch (PDOException $e) {
                 die("Connectie mislukt:" . $e->getMessage());
             }
-
+            
+            // bestaat het? sla klantgegevens op in sessievariabelen
             if ($klantID) {
                 $_SESSION['klant_id'] = $klantID['ID'];
                 $_SESSION['klant_naam'] = $klantID['Naam'];
+                // succesbericht wordt weergegeven als het gelukt is
                 $_SESSION['success'] = "Login successful";
 
+                // de gebruiker wordt doorgestuurd naar 'donkey_client.php' de klant pagina
                 header('location: ../donkey_client.php');
                 exit();
+                // bestaat het niet? wordt er een error weergegeven
             } else {
                 echo "Login failed. Invalid credentials.";
             }
         }
     }
 
+    // controleert of een HTML-formulier is verzonden.
     if(isset($_POST['submit'])) {
+        // zo ja? haal de waarden van de "Naam" en "Wachtwoord" invoervelden op
         $Naam = $_POST['Naam'];
         $Wachtwoord = $_POST['Wachtwoord'];
 
+        // maak een nieuw object aan van de klasse "klantLogin"
         $klant1 = new klantLogin();
+        // de methode "loginKlant" wordt aangeroepen
         $klant1->loginKlant($Naam, $Wachtwoord); 
     } 
 ?>
