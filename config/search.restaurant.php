fc82bf73fdbe2d19e26b8dcad20b9df7e620b22e
Khaqan <khaqan.10@outlook.com>
2023-10-23 11:50:39 +0200

    Search function is changed to 1 file

--- config/search.restaurant.php
+++ config/search.restaurant.php
@@ -5,10 +5,17 @@
 </head>
 <body>
 <h1>zoek reservering</h1>
-<form action="search.restaurant2.php" method="post">
+<form action="" method="post">
     <label for="ID">ID:</label>
     <input type="text" id = "ID" name="ID"><br/>
     <input type="submit">
 </form>
 </body>
 </html>
+<?php
+require_once "classes/restaurant.class.php";
+
+$ID = $_POST["ID"];
+$ID1 = new restaurants($ID);
+$ID1->searchrestaurant();
+?>
